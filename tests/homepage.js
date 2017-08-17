28b20f9313eb82560d2fcb6794960dfe8d641e68
Shaopeng Zhang <szhangpitt@gmail.com>
2017-08-16 21:20:45 -0400

    minimal settings

---
+++ tests/homepage.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "Demo test Google": function(client) {
+    client
+      .url(`http://www.openfigi.com/search`)
+      .waitForElementVisible(`button[type="submit"]`, 2000)
+      .assert.title(`Search | OpenFIGI`)
+      .assert.visible(`input[ng-model="_.simpleSearchString"]`)
+      .setValue(`input[ng-model="_.simpleSearchString"]`, `ibm`)
+      // .waitForElementVisible(`input[ng-model="_.simpleSearchString"]`, 1000)
+      .click(`button[type="submit"]`)
+      .pause(2000)
+      .assert.containsText(`body`, `Rows per page`)
+      .end();
+  },
+};
